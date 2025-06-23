1cc0284bcd30bfa9fc3e10b07b0646af0f1b9439
רחל גרשון | Rachel Gershon <213060965@gov.il>
2025-06-23 15:22:54 +0300

    update server

--- server/utils/storage.py
+++ server/utils/storage.py
@@ -6,13 +6,6 @@ def upload_file(bucket_name, file):
     blob = bucket.blob(file.filename)
     blob.upload_from_file(file)
 
-def upload_file_from_path(bucket_name, file_path):
-    client = storage.Client()
-    bucket = client.bucket(bucket_name)
-    blob = bucket.blob(os.path.basename(file_path))
-    with open(file_path, "rb") as f:
-        blob.upload_from_file(f)
-
 def get_files(bucket_name):
     client = storage.Client()
     bucket = client.bucket(bucket_name)
