96dbcaebcc8ef4299c38fcce47064b7331019ca6
רחל גרשון | Rachel Gershon <213060965@gov.il>
2025-06-24 13:52:10 +0300

    update server

--- server/app/utils/database.py
+++ server/app/utils/database.py
@@ -4,10 +4,10 @@ from app.utils.secret import get_secret
 
 def insert_data(name, value, project_id):
     connector = Connector()
-    db_user = get_secret("database-user", project_id)
-    db_password = get_secret("database-password", project_id)
-    db_name = get_secret("database-name", project_id)
-    connection_name = get_secret("database-host", project_id) 
+    db_user = get_secret("db-user", project_id)
+    db_password = get_secret("db-password", project_id)
+    db_name = get_secret("db-name", project_id)
+    connection_name = get_secret("db-host", project_id) 
     def getconn():
        return connector.connect(
         connection_name,
