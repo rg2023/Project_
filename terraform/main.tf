ec1e5812299f344727b2f5015e7853363dd6940d
רחל גרשון | Rachel Gershon <213060965@gov.il>
2025-06-24 11:52:42 +0300

    update server

--- terraform/main.tf
+++ terraform/main.tf
@@ -61,7 +61,7 @@ module "secret-manager" {
       {
       name                  = "${var.db_name}-user"
       automatic_replication = true
-      secret_data          = "root"
+      secret_data          = "default"
     },
     {
       name                  = "${var.db_name}-name"
